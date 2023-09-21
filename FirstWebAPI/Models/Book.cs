2b793f9bda255414424a5bd8cf78faf6e444447c
Sathwik Kumar Mellachervu <sathwikkumar.m@gavstech.com>
2023-09-21 14:57:07 +0530

    Included book and BookController

---
+++ FirstWebAPI/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstWebAPI.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
