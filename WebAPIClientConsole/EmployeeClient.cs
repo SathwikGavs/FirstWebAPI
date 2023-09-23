5c50ae536175fcacd7b8c3151e65159fe93473b5
SathwikGavs <145546822+SathwikGavs@users.noreply.github.com>
2023-09-23 13:54:53 +0530

    Update EmployeeClient.cs

--- WebAPIClientConsole/EmployeeClient.cs
+++ WebAPIClientConsole/EmployeeClient.cs
@@ -17,37 +17,59 @@ namespace WebAPIClientConsole
         {
             using(var client = new HttpClient())
             {
-                //client.BaseAddress = uri;
-
-                //HttpResponseMessage response = await client.GetAsync("GetAllEmployee");
-                //response.EnsureSuccessStatusCode();
-                //if(response.IsSuccessStatusCode )
-                //{
-                //    String x = await response.Content.ReadAsStringAsync();
-                //    await Console.Out.WriteLineAsync(x);
-                //}
-
-
-                client.BaseAddress = uri;
-                List<Employee> employees = new List<Employee>();
-                client.DefaultRequestHeaders
-                    .Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                        client.BaseAddress = uri;
+        List<Employee> employees = new List<Employee>();
+        client.DefaultRequestHeaders
+            .Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
 
 
-                //HttpGet:
-                HttpResponseMessage response = await client.GetAsync("GetAllEmployees");
-                response.EnsureSuccessStatusCode();
-                if (response.IsSuccessStatusCode)
-                {
-                    String json = await response.Content.ReadAsStringAsync();
-                    //install Newtonsoft.Json using package installer
-                    employees = JsonConvert.DeserializeObject<List<Employee>>(json);
-                    foreach (Employee emp in employees)
-                    {
-                        await Console.Out.WriteLineAsync($"{emp.EmployeeId},{emp.FirstName},{emp.LastName}");
-                    }
-                }
+        //HttpGet:
+        HttpResponseMessage response = await client.GetAsync("GetAllEmployees");
+        response.EnsureSuccessStatusCode();
+        if (response.IsSuccessStatusCode)
+        {
+            String json = await response.Content.ReadAsStringAsync();
+            //install Newtonsoft.Json using package installer
+            employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+            foreach (Employee emp in employees)
+            {
+                await Console.Out.WriteLineAsync($"{emp.EmpId},{emp.FirstName},{emp.LastName}");
+            }
+        }
+    }
+}
+public static async Task AddnewEmployee()
+{
+    using (var client = new HttpClient())
+    {
+        client.BaseAddress = uri;
+        client.DefaultRequestHeaders.Accept
+            .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        Employee employee = new Employee()
+        {
+            FirstName = "William",
+            LastName = "John",
+            City = "Nyc",
+            BirthDate = new DateTime(1980, 01, 01),
+            HireDate = new DateTime(2000, 01, 01),
+            Title = "Manager"
+        };
+        var myContent = JsonConvert.SerializeObject(employee);
+        var buffer = Encoding.UTF8.GetBytes(myContent);
+        var byteContent = new ByteArrayContent(buffer);
+        byteContent.Headers.ContentType =
+            new MediaTypeHeaderValue("application/json");
+        //HttpPost:
+        HttpResponseMessage response =
+            await client.PostAsync("AddNewEmployees", byteContent);
+        response.EnsureSuccessStatusCode();
+        if (response.IsSuccessStatusCode)
+        {
+            await Console.Out.WriteLineAsync(response.StatusCode.ToString());
+        }
+               
+
             }
         }
 
